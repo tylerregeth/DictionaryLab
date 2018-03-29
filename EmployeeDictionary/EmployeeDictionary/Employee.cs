74f6db07422317227ffb78f4519f3021f321b83a
tylerregeth <37908334+tylerregeth@users.noreply.github.com>
2018-03-29 10:59:35 -0500

    First commit
    Testing first commit

---
+++ EmployeeDictionary/EmployeeDictionary/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDictionary
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int Id { get; set; }
+
+        public override string ToString()
+        {
+            return Id.ToString() + ": " + FirstName + " " + LastName;
+        }
+    }
+}
